3094820b3a1092b3c617d624fb40caca6462ec62
Ankit Chauhan <68211364+Mystery137@users.noreply.github.com>
2022-05-20 02:00:00 -0700

    Add files via upload
    uploaded html, css , javascript files for this project

---
+++ index.js
@@ -0,0 +1,64 @@
+const timer = document.getElementById("stopwatch");
+
+
+var Stopwatch = true;
+var sec = 0;
+var min = 0;
+var hr = 0;
+
+function startbutton() {
+    if (Stopwatch == true) {
+        Stopwatch = false;
+        TimeCycle();
+    }
+}
+
+function stopbutton() {
+    if (Stopwatch == false) {
+        Stopwatch = true;
+    }
+}
+
+function resetbutton() {
+    timer.innerHTML = "00:00:00";
+    Stopwatch = true;
+    sec = 0;
+    min = 0;
+    hr = 0;
+}
+
+function TimeCycle() {
+    if (Stopwatch == false) {
+        sec = parseInt(sec);
+        min = parseInt(min);
+        hr = parseInt(hr);
+        sec = sec + 1;
+
+        if (sec == 60) {
+            min = min + 1;
+            sec = 0;
+        }
+        if (min == 60) {
+            hr = hr + 1;
+            min = 0;
+            sec = 0;
+        }
+
+        if (sec < 10) {
+            sec = "0" + sec;
+        }
+
+        if (min < 10) {
+            min = "0" + min;
+        }
+
+        if (hr < 10) {
+            hr = "0" + hr;
+        }
+
+        timer.innerHTML = hr + ":" + min + ":" + sec;
+
+        setTimeout("TimeCycle()", 1000);
+
+    }
+}
